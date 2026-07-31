7c2eb187b838811b8a3a5a2bd10ddf702810a156
Dedi <dedi@tki.local>
2026-07-31 17:24:23 +0700

    hapus inventaris app and staff

--- inventaris/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Gitar
-
-admin.site.register(Gitar)
--- inventaris/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InventarisConfig(AppConfig):
-    name = 'inventaris'
